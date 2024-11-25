1c6b1f6bab85b55ad5f254b65ebdf4cf73d511d9
Kathlene Magnus <kathlene.hurt@gmail.com>
2024-11-25 15:00:52 -0600

    More M extension fixes

--- core/inst_handlers/rv64/m/RvmInsts.cpp
+++ core/inst_handlers/rv64/m/RvmInsts.cpp
@@ -1,8 +1,10 @@
 #include "core/inst_handlers/rv64/m/RvmInsts.hpp"
 #include "core/inst_handlers/inst_helpers.hpp"
-#include "core/ActionGroup.hpp"
 #include "include/ActionTags.hpp"
 #include "core/ActionGroup.hpp"
+#include "core/AtlasState.hpp"
+#include "core/AtlasInst.hpp"
+
 namespace atlas
 {
     void RvmInsts::getInstHandlers(std::map<std::string, Action> & inst_handlers)
@@ -48,25 +50,6 @@ namespace atlas
                                   nullptr, "remw", ActionTags::EXECUTE_TAG));
     }
 
-
-
-    ActionGroup* RvmInsts::remu_64_handler(atlas::AtlasState* state)
-    {
-        const AtlasInstPtr & insn = state->getCurrentInst();
-        const uint64_t rs1_val = insn->getRs1()->read();
-        const uint64_t rs2_val = insn->getRs2()->read();
-
-        reg_t lhs = zext_xlen(rs1_val);
-        reg_t rhs = zext_xlen(rs2_val);
-        if (rhs == 0) {
-            insn->getRd()->write(sext_xlen(lhs));
-        } else {
-            insn->getRd()->write(sext_xlen(lhs % rhs));
-        }
-
-        return nullptr;
-    }
-
     ActionGroup* RvmInsts::div_64_handler(atlas::AtlasState* state)
     {
         const AtlasInstPtr & insn = state->getCurrentInst();
@@ -83,6 +66,125 @@ namespace atlas
         } else {
             insn->getRd()->write(sext_xlen(lhs / rhs));
         }
+
+        return nullptr;
+    }
+
+    ActionGroup* RvmInsts::divu_64_handler(atlas::AtlasState* state)
+    {
+        const AtlasInstPtr & insn = state->getCurrentInst();
+        const uint64_t rs1_val = insn->getRs1()->read();
+        const uint64_t rs2_val = insn->getRs2()->read();
+
+        reg_t lhs = zext_xlen(rs1_val);
+        reg_t rhs = zext_xlen(rs2_val);
+        if (rhs == 0) {
+            insn->getRd()->write(UINT64_MAX);
+        } else {
+            insn->getRd()->write(sext_xlen(lhs / rhs));
+        }
+
+	return nullptr;
+    }
+
+    ActionGroup* RvmInsts::divuw_64_handler(atlas::AtlasState* state)
+    {
+        const AtlasInstPtr & insn = state->getCurrentInst();
+        const uint64_t rs1_val = insn->getRs1()->read();
+        const uint64_t rs2_val = insn->getRs2()->read();
+
+        reg_t lhs = zext32(rs1_val);
+        reg_t rhs = zext32(rs2_val);
+        if (rhs == 0) {
+            insn->getRd()->write(UINT64_MAX);
+        } else {
+            insn->getRd()->write(sext32(lhs / rhs));
+        }
+
+        return nullptr;
+    }
+
+    ActionGroup* RvmInsts::divw_64_handler(atlas::AtlasState* state)
+    {
+        const AtlasInstPtr & insn = state->getCurrentInst();
+        const uint64_t rs1_val = insn->getRs1()->read();
+        const uint64_t rs2_val = insn->getRs2()->read();
+
+        sreg_t lhs = sext32(rs1_val);
+        sreg_t rhs = sext32(rs2_val);
+        if (rhs == 0) {
+            insn->getRd()->write(UINT64_MAX);
+        } else {
+            insn->getRd()->write(sext32(lhs / rhs));
+        }
+
+        return nullptr;
+    }
+
+    ActionGroup* RvmInsts::mul_64_handler(atlas::AtlasState* state)
+    {
+        const AtlasInstPtr & insn = state->getCurrentInst();
+
+        const uint64_t rs1_val = insn->getRs1()->read();
+        const uint64_t rs2_val = insn->getRs2()->read();
+        const uint64_t rd_val = rs1_val * rs2_val;
+        insn->getRd()->write(rd_val);
+
+        return nullptr;
+    }
+
+    ActionGroup* RvmInsts::mulh_64_handler(atlas::AtlasState* state)
+    {
+        const AtlasInstPtr & insn = state->getCurrentInst();
+        const uint64_t rs1_val = insn->getRs1()->read();
+        const uint64_t rs2_val = insn->getRs2()->read();
+
+        if (xlen == 64) {
+            insn->getRd()->write(mulh(rs1_val, rs2_val));
+        } else {
+            insn->getRd()->write(sext32((sext32(rs1_val) * sext32(rs2_val)) >> 32));
+        }
+
+        return nullptr;
+    }
+
+    ActionGroup* RvmInsts::mulhsu_64_handler(atlas::AtlasState* state)
+    {
+        const AtlasInstPtr & insn = state->getCurrentInst();
+        const uint64_t rs1_val = insn->getRs1()->read();
+        const uint64_t rs2_val = insn->getRs2()->read();
+
+        if (xlen == 64) {
+            insn->getRd()->write(mulhsu(rs1_val, rs2_val));
+        } else {
+            insn->getRd()->write(sext32((sext32(rs1_val) * reg_t((uint32_t)rs2_val)) >> 32));
+        }
+
+        return nullptr;
+    }
+
+    ActionGroup* RvmInsts::mulhu_64_handler(atlas::AtlasState* state)
+    {
+	const AtlasInstPtr & insn = state->getCurrentInst();
+        const uint64_t rs1_val = insn->getRs1()->read();
+        const uint64_t rs2_val = insn->getRs2()->read();
+
+        if (xlen == 64) {
+            insn->getRd()->write(mulhu(rs1_val, rs2_val));
+        } else {
+            insn->getRd()->write(sext32(((uint64_t)(uint32_t)rs1_val * (uint64_t)(uint32_t)rs2_val) >> 32));
+        }
+
+	return nullptr;
+    }
+
+    ActionGroup* RvmInsts::mulw_64_handler(atlas::AtlasState* state)
+    {
+        const AtlasInstPtr & insn = state->getCurrentInst();
+
+        const uint64_t rs1_val = insn->getRs1()->read();
+        const uint64_t rs2_val = insn->getRs2()->read();
+        insn->getRd()->write(sext32(rs1_val * rs2_val));
 
         return nullptr;
     }
@@ -107,18 +209,18 @@ namespace atlas
         return nullptr;
     }
 
-    ActionGroup* RvmInsts::divw_64_handler(atlas::AtlasState* state)
+    ActionGroup* RvmInsts::remu_64_handler(atlas::AtlasState* state)
     {
         const AtlasInstPtr & insn = state->getCurrentInst();
         const uint64_t rs1_val = insn->getRs1()->read();
         const uint64_t rs2_val = insn->getRs2()->read();
 
-        sreg_t lhs = sext32(rs1_val);
-        sreg_t rhs = sext32(rs2_val);
+        reg_t lhs = zext_xlen(rs1_val);
+        reg_t rhs = zext_xlen(rs2_val);
         if (rhs == 0) {
-            insn->getRd()->write(UINT64_MAX);
+            insn->getRd()->write(sext_xlen(lhs));
         } else {
-            insn->getRd()->write(sext32(lhs / rhs));
+            insn->getRd()->write(sext_xlen(lhs % rhs));
         }
 
         return nullptr;
@@ -141,73 +243,20 @@ namespace atlas
         return nullptr;
     }
 
-    ActionGroup* RvmInsts::mulh_64_handler(atlas::AtlasState* state)
+    ActionGroup* RvmInsts::remw_64_handler(atlas::AtlasState* state)
     {
         const AtlasInstPtr & insn = state->getCurrentInst();
         const uint64_t rs1_val = insn->getRs1()->read();
         const uint64_t rs2_val = insn->getRs2()->read();
 
-        if (xlen == 64) {
-            insn->getRd()->write(mulh(rs1_val, rs2_val));
-        } else {
-            insn->getRd()->write(sext32((sext32(rs1_val) * sext32(rs2_val)) >> 32));
-        }
-
-        return nullptr;
-    }
-
-    ActionGroup* RvmInsts::divuw_64_handler(atlas::AtlasState* state)
-    {
-        const AtlasInstPtr & insn = state->getCurrentInst();
-        const uint64_t rs1_val = insn->getRs1()->read();
-        const uint64_t rs2_val = insn->getRs2()->read();
-
-        reg_t lhs = zext32(rs1_val);
-        reg_t rhs = zext32(rs2_val);
+        sreg_t lhs = sext32(rs1_val);
+        sreg_t rhs = sext32(rs2_val);
         if (rhs == 0) {
-            insn->getRd()->write(UINT64_MAX);
+            insn->getRd()->write(lhs);
         } else {
-            insn->getRd()->write(sext32(lhs / rhs));
+            insn->getRd()->write(sext32(lhs % rhs));
         }
 
-        return nullptr;
-    }
-
-    ActionGroup* RvmInsts::mulw_64_handler(atlas::AtlasState* state)
-    {
-        const AtlasInstPtr & insn = state->getCurrentInst();
-
-        const uint64_t rs1_val = insn->getRs1()->read();
-        const uint64_t rs2_val = insn->getRs2()->read();
-        insn->getRd()->write(sext32(rs1_val * rs2_val));
-
-        return nullptr;
-    }
-
-    ActionGroup* RvmInsts::mul_64_handler(atlas::AtlasState* state)
-    {
-        const AtlasInstPtr & insn = state->getCurrentInst();
-
-        const uint64_t rs1_val = insn->getRs1()->read();
-        const uint64_t rs2_val = insn->getRs2()->read();
-        const uint64_t rd_val = rs1_val * rs2_val;
-        insn->getRd()->write(rd_val);
-
-        return nullptr;
-    }
-
-    ActionGroup* RvmInsts::mulhsu_64_handler(atlas::AtlasState* state)
-    {
-        const AtlasInstPtr & insn = state->getCurrentInst();
-        const uint64_t rs1_val = insn->getRs1()->read();
-        const uint64_t rs2_val = insn->getRs2()->read();
-
-        if (xlen == 64) {
-            insn->getRd()->write(mulhsu(rs1_val, rs2_val));
-        } else {
-            insn->getRd()->write(sext32((sext32(rs1_val) * reg_t((uint32_t)rs2_val)) >> 32));
-        }
-
-        return nullptr;
+	return nullptr;
     }
 } // namespace atlas
