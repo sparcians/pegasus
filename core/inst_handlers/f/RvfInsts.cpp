12a6dbc79d53118fa4426c7ff828fac9f9b82c3f
Kathlene Magnus <kathlene.hurt@gmail.com>
2024-12-20 15:22:39 -0600

    Ran clangformat

--- core/inst_handlers/f/RvfInsts.cpp
+++ core/inst_handlers/f/RvfInsts.cpp
@@ -1,7 +1,8 @@
 #include "core/inst_handlers/f/RvfInsts.hpp"
 #include "include/ActionTags.hpp"
 #include "core/ActionGroup.hpp"
-extern "C" {
+extern "C"
+{
 #include "source/include/softfloat.h"
 }
 
