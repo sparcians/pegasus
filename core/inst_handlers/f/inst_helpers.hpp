e8cccc7eb02e76f292bde3b777c7a8d1b2a3f551
liya zhu <lzhu@mips.com>
2024-12-20 09:07:56 -0800

    add f extension

--- core/inst_handlers/f/inst_helpers.hpp
+++ core/inst_handlers/f/inst_helpers.hpp
@@ -7,17 +7,30 @@
 
 namespace atlas
 {
+<<<<<<< HEAD
     constexpr uint64_t rv64_l32_mask = 0x0000FFFF;
     
     inline uint_fast8_t getRM(const AtlasInstPtr & inst)
     {
         const uint64_t static_rm = inst->getRM();
+=======
+    uint_fast8_t getRM(const AtlasInstPtr & inst)
+    {
+        uint64_t static_rm = inst->getRM();
+>>>>>>> ba8f11c (add f extension)
         if (static_rm == 7) // RM field "DYN"
         {
             // TODO: use fcsr::frm
             return 0;
         }
+<<<<<<< HEAD
         return static_rm;
+=======
+        else
+        {
+            return static_rm;
+        }
+>>>>>>> ba8f11c (add f extension)
     }
 
     template <typename T> ActionGroup* compute_address_handler(AtlasState* state)
@@ -33,17 +46,32 @@ namespace atlas
         return nullptr;
     }
 
+<<<<<<< HEAD
     template <typename RV, typename SIZE, bool LOAD>
     ActionGroup* float_ls_handler(atlas::AtlasState* state)
     {
         static_assert(std::is_same<RV, RV64>::value || std::is_same<RV, RV32>::value);
         static_assert(std::is_same<SIZE, SP>::value || std::is_same<SIZE, DP>::value);
+=======
+    using S = uint32_t;
+    using D = uint64_t;
+
+    template <typename RV, typename SIZE>
+    ActionGroup* float_ls_handler(atlas::AtlasState* state, bool load)
+    {
+        static_assert(std::is_same<RV, RV64>::value || std::is_same<RV, RV32>::value);
+        static_assert(std::is_same<SIZE, S>::value || std::is_same<SIZE, D>::value);
+>>>>>>> ba8f11c (add f extension)
         static_assert(sizeof(RV) >= sizeof(SIZE));
 
         const AtlasInstPtr & inst = state->getCurrentInst();
         const RV paddr = state->getTranslationState()->getTranslationResult().getPaddr();
 
+<<<<<<< HEAD
         if constexpr (LOAD)
+=======
+        if (load)
+>>>>>>> ba8f11c (add f extension)
         {
             inst->getRd()->write(state->readMemory<SIZE>(paddr));
         }
