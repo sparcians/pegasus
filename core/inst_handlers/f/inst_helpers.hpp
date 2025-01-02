a0ae623bd1836a112f3ed574d93a1cb3c14a2e84
liya zhu <lzhu@mips.com>
2025-01-02 14:03:44 -0800

    add d extension

--- core/inst_handlers/f/inst_helpers.hpp
+++ core/inst_handlers/f/inst_helpers.hpp
@@ -3,34 +3,61 @@
 #include "core/AtlasState.hpp"
 #include "core/AtlasInst.hpp"
 #include "include/AtlasUtils.hpp"
+#include "include/CSRBitMasks64.hpp"
 #include "mavis/OpcodeInfo.h"
+#include "arch/register_macros.hpp"
+
+extern "C"
+{
+#include "source/include/softfloat.h"
+}
+
 
 namespace atlas
 {
-<<<<<<< HEAD
     constexpr uint64_t rv64_l32_mask = 0x0000FFFF;
     
-    inline uint_fast8_t getRM(const AtlasInstPtr & inst)
-    {
-        const uint64_t static_rm = inst->getRM();
-=======
-    uint_fast8_t getRM(const AtlasInstPtr & inst)
+    inline uint_fast8_t getRM(const AtlasInstPtr & inst, AtlasState* state)
     {
         uint64_t static_rm = inst->getRM();
->>>>>>> ba8f11c (add f extension)
         if (static_rm == 7) // RM field "DYN"
         {
-            // TODO: use fcsr::frm
-            return 0;
+            static_rm = READ_CSR_REG(FRM);
         }
-<<<<<<< HEAD
         return static_rm;
-=======
-        else
-        {
-            return static_rm;
-        }
->>>>>>> ba8f11c (add f extension)
+    }
+    
+    // TODO: use official WRITE_CSR_FIELD when it's fixed.
+    #ifdef WRITE_CSR_FIELD
+    #undef WRITE_CSR_FIELD
+    #define WRITE_CSR_FIELD(reg_name, field_name, field_value)                                     \
+    {                                                                                              \
+        const auto old_value =                                                                     \
+            state->getCsrRegister(atlas::CSR::reg_name::reg_num)->dmiRead<uint64_t>();             \
+        WRITE_CSR_REG(reg_name,                                                                    \
+                      (old_value & ~reg_name ## _64_bitmasks::field_name) |                        \
+                      (field_value & reg_name ## _64_bitmasks::field_name));                       \
+    }
+    #endif
+    
+    inline void update_csr(AtlasState* state)
+    {
+        // FFLAGS
+        WRITE_CSR_FIELD(FFLAGS, NX, softfloat_exceptionFlags & softfloat_flag_inexact);
+        WRITE_CSR_FIELD(FFLAGS, UF, softfloat_exceptionFlags & softfloat_flag_underflow);
+        WRITE_CSR_FIELD(FFLAGS, OF, softfloat_exceptionFlags & softfloat_flag_overflow);
+        WRITE_CSR_FIELD(FFLAGS, DZ, softfloat_exceptionFlags & softfloat_flag_infinite);
+        WRITE_CSR_FIELD(FFLAGS, NV, softfloat_exceptionFlags & softfloat_flag_invalid);
+
+        // FCSR
+        WRITE_CSR_FIELD(FCSR, NX, softfloat_exceptionFlags & softfloat_flag_inexact);
+        WRITE_CSR_FIELD(FCSR, UF, softfloat_exceptionFlags & softfloat_flag_underflow);
+        WRITE_CSR_FIELD(FCSR, OF, softfloat_exceptionFlags & softfloat_flag_overflow);
+        WRITE_CSR_FIELD(FCSR, DZ, softfloat_exceptionFlags & softfloat_flag_infinite);
+        WRITE_CSR_FIELD(FCSR, NV, softfloat_exceptionFlags & softfloat_flag_invalid);
+
+        // clear softfloat flag variable
+        softfloat_exceptionFlags = 0;
     }
 
     template <typename T> ActionGroup* compute_address_handler(AtlasState* state)
@@ -46,32 +73,17 @@ namespace atlas
         return nullptr;
     }
 
-<<<<<<< HEAD
     template <typename RV, typename SIZE, bool LOAD>
     ActionGroup* float_ls_handler(atlas::AtlasState* state)
     {
         static_assert(std::is_same<RV, RV64>::value || std::is_same<RV, RV32>::value);
         static_assert(std::is_same<SIZE, SP>::value || std::is_same<SIZE, DP>::value);
-=======
-    using S = uint32_t;
-    using D = uint64_t;
-
-    template <typename RV, typename SIZE>
-    ActionGroup* float_ls_handler(atlas::AtlasState* state, bool load)
-    {
-        static_assert(std::is_same<RV, RV64>::value || std::is_same<RV, RV32>::value);
-        static_assert(std::is_same<SIZE, S>::value || std::is_same<SIZE, D>::value);
->>>>>>> ba8f11c (add f extension)
         static_assert(sizeof(RV) >= sizeof(SIZE));
 
         const AtlasInstPtr & inst = state->getCurrentInst();
         const RV paddr = state->getTranslationState()->getTranslationResult().getPaddr();
 
-<<<<<<< HEAD
         if constexpr (LOAD)
-=======
-        if (load)
->>>>>>> ba8f11c (add f extension)
         {
             inst->getRd()->write(state->readMemory<SIZE>(paddr));
         }
