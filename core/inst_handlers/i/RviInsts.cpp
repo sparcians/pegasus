6c727724f379996b2423f1a5118a327292d524ac
Kathlene Magnus <kathlene.hurt@gmail.com>
2025-01-16 10:59:15 -0600

    Removed duplicate li handler

--- core/inst_handlers/i/RviInsts.cpp
+++ core/inst_handlers/i/RviInsts.cpp
@@ -576,17 +576,6 @@ namespace atlas
         return nullptr;
     }
 
-    ActionGroup* RviInsts::li_64_handler(atlas::AtlasState* state)
-    {
-        const AtlasInstPtr & insn = state->getCurrentInst();
-
-        const uint32_t IMM_SIZE = 12;
-        const uint64_t imm = insn->getSignExtendedImmediate<RV64, IMM_SIZE>();
-        insn->getRd()->dmiWrite(imm);
-
-        return nullptr;
-    }
-
     ActionGroup* RviInsts::srli_64_handler(atlas::AtlasState* state)
     {
         const AtlasInstPtr & insn = state->getCurrentInst();
