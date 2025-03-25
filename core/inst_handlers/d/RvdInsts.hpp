67e2ab946d8a2e5a0e60dade261248347a117548
Liya Zhu <lzhu@mips.com>
2025-03-25 17:54:01 -0500

    add RV32 support for F/D extension (#89)
    Co-authored-by: lzhu-pub <julia.in.midkingdom@gmail.com>

--- core/inst_handlers/d/RvdInsts.hpp
+++ core/inst_handlers/d/RvdInsts.hpp
@@ -10,56 +10,46 @@ namespace atlas
     class Action;
     class ActionGroup;
 
-    class RvdInsts : RvfInstsBase
+    class RvdInsts : public RvfInstsBase
     {
       public:
-        using base_type = RvdInsts;
-
         template <typename XLEN>
         static void getInstComputeAddressHandlers(std::map<std::string, Action> & inst_handlers);
         template <typename XLEN>
         static void getInstHandlers(std::map<std::string, Action> & inst_handlers);
 
       private:
-        ActionGroup* fadd_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fclass_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_d_l_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_d_lu_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_d_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_d_w_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_d_wu_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_l_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_lu_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_s_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_w_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_wu_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fdiv_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* feq_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fld_64_compute_address_handler(atlas::AtlasState* state);
-        ActionGroup* fld_64_handler(atlas::AtlasState* state);
-        ActionGroup* fle_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* flt_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* flw_64_compute_address_handler(atlas::AtlasState* state);
-        ActionGroup* flw_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmadd_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmax_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmin_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmsub_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmul_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmv_d_x_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmv_w_x_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmv_x_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmv_x_w_64_handler(atlas::AtlasState* state);
-        ActionGroup* fnmadd_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fnmsub_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsd_64_compute_address_handler(atlas::AtlasState* state);
-        ActionGroup* fsd_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsgnj_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsgnjn_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsgnjx_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsqrt_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsub_d_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsw_64_compute_address_handler(atlas::AtlasState* state);
-        ActionGroup* fsw_64_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fadd_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fclass_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_d_l_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_d_lu_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_d_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_d_w_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_d_wu_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_l_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_lu_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_s_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_w_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_wu_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fdiv_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* feq_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fle_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* flt_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmadd_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmax_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmin_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmsub_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmul_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmv_d_x_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmv_w_x_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmv_x_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmv_x_w_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fnmadd_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fnmsub_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsgnj_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsgnjn_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsgnjx_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsqrt_d_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsub_d_handler(atlas::AtlasState* state);
     };
 } // namespace atlas
--- core/inst_handlers/f/RvfInsts.hpp
+++ core/inst_handlers/f/RvfInsts.hpp
@@ -10,48 +10,42 @@ namespace atlas
     class Action;
     class ActionGroup;
 
-    class RvfInsts : RvfInstsBase
+    class RvfInsts : public RvfInstsBase
     {
       public:
-        using base_type = RvfInsts;
-
         template <typename XLEN>
         static void getInstComputeAddressHandlers(std::map<std::string, Action> & inst_handlers);
         template <typename XLEN>
         static void getInstHandlers(std::map<std::string, Action> & inst_handlers);
 
       private:
-        ActionGroup* fadd_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fclass_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_l_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_lu_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_s_l_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_s_lu_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_s_w_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_s_wu_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_w_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fcvt_wu_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fdiv_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* feq_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fle_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* flt_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* flw_64_compute_address_handler(atlas::AtlasState* state);
-        ActionGroup* flw_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmadd_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmax_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmin_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmsub_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmul_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmv_w_x_64_handler(atlas::AtlasState* state);
-        ActionGroup* fmv_x_w_64_handler(atlas::AtlasState* state);
-        ActionGroup* fnmadd_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fnmsub_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsgnj_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsgnjn_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsgnjx_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsqrt_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsub_s_64_handler(atlas::AtlasState* state);
-        ActionGroup* fsw_64_compute_address_handler(atlas::AtlasState* state);
-        ActionGroup* fsw_64_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fadd_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fclass_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_l_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_lu_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_s_l_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_s_lu_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_s_w_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_s_wu_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_w_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fcvt_wu_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fdiv_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* feq_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fle_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* flt_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmadd_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmax_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmin_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmsub_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmul_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmv_w_x_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fmv_x_w_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fnmadd_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fnmsub_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsgnj_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsgnjn_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsgnjx_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsqrt_s_handler(atlas::AtlasState* state);
+        template <typename XLEN> ActionGroup* fsub_s_handler(atlas::AtlasState* state);
     };
 } // namespace atlas
