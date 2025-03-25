6f0e0e1cd53c2fa0b3142bef0321ddf62ac93a80
Colby Nyce <iheartridleyman@gmail.com>
2025-03-25 11:12:00 -0500

    Run clang-format

--- core/AtlasState.cpp
+++ core/AtlasState.cpp
@@ -688,10 +688,14 @@ namespace atlas
 
             std::cout << "AtlasState::boot()\n";
             std::cout << std::hex;
-            std::cout << "\tMHARTID: 0x" << state->getCsrRegister(MHARTID)->dmiRead<uint64_t>() << std::endl;
-            std::cout << "\tMISA:    0x" << state->getCsrRegister(MISA)->dmiRead<uint64_t>()    << std::endl;
-            std::cout << "\tMSTATUS: 0x" << state->getCsrRegister(MSTATUS)->dmiRead<uint64_t>() << std::endl;
-            std::cout << "\tSSTATUS: 0x" << state->getCsrRegister(SSTATUS)->dmiRead<uint64_t>() << std::endl;
+            std::cout << "\tMHARTID: 0x" << state->getCsrRegister(MHARTID)->dmiRead<uint64_t>()
+                      << std::endl;
+            std::cout << "\tMISA:    0x" << state->getCsrRegister(MISA)->dmiRead<uint64_t>()
+                      << std::endl;
+            std::cout << "\tMSTATUS: 0x" << state->getCsrRegister(MSTATUS)->dmiRead<uint64_t>()
+                      << std::endl;
+            std::cout << "\tSSTATUS: 0x" << state->getCsrRegister(SSTATUS)->dmiRead<uint64_t>()
+                      << std::endl;
             std::cout << std::dec;
         }
 
