769baec08e7a7a0eb59a7efaa1c8c86185f53ed2
Kathlene Magnus <kathlene.hurt@gmail.com>
2025-04-16 08:54:05 -0500

    Use Instruction Handler Name Instead of Mnemonic (#123)
    In `AtlasExtractor`, an instruction's Action Group is initialized with
    its instruction handler Action(s). The instruction's instruction handler
    name, defined in the Atlas uarch JSON files, should be used to find the
    instruction handler Action - NOT the instruction's mnemonic. Fixed this
    bug and then cleaned up the instruction handler names in the JSONs.

--- scripts/insts/RVZBA_INST.py
+++ scripts/insts/RVZBA_INST.py
@@ -1,16 +1,16 @@
 RV32ZBA_INST = [
-    {'mnemonic': 'sh1add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_32', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
-    {'mnemonic': 'sh2add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_32', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
-    {'mnemonic': 'sh3add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_32', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
+    {'mnemonic': 'sh1add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_32', 'memory': False, 'cof': False},
+    {'mnemonic': 'sh2add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_32', 'memory': False, 'cof': False},
+    {'mnemonic': 'sh3add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_32', 'memory': False, 'cof': False},
 ]
 
 RV64ZBA_INST = [
-    {'mnemonic': 'add.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
-    {'mnemonic': 'sh1add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
-    {'mnemonic': 'sh1add.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
-    {'mnemonic': 'sh2add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
-    {'mnemonic': 'sh2add.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
-    {'mnemonic': 'sh3add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
-    {'mnemonic': 'sh3add.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
-    {'mnemonic': 'slli.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False, 'vdeleg': False, 'vector': False, 'float': False, 'maskable': False, 'segment': False, 'indexed': False, 'atomic': False, 'crypto': False},
+    {'mnemonic': 'add.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False},
+    {'mnemonic': 'sh1add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False},
+    {'mnemonic': 'sh1add.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False},
+    {'mnemonic': 'sh2add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False},
+    {'mnemonic': 'sh2add.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False},
+    {'mnemonic': 'sh3add', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False},
+    {'mnemonic': 'sh3add.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False},
+    {'mnemonic': 'slli.uw', 'handler': 'nop', 'cost': 1, 'tags': 'B_EXT_64', 'memory': False, 'cof': False},
 ]
