6177541177ee9d97f50bf89ad4d8db5077ac856b
liya zhu <lzhu@mips.com>
2025-05-08 15:10:31 -0700

    v ext: refactoring vcs and via tests

--- test/sim/vcs/Vcs_test.cpp
+++ test/sim/vcs/Vcs_test.cpp
@@ -5,48 +5,100 @@ class VcsInstructionTester : public AtlasInstructionTester
 {
 
   public:
+    using VLEN = std::array<uint8_t, 8>;
+    using XLEN = uint64_t;
+
     VcsInstructionTester() = default;
 
     void testVsetvl()
     {
-        const uint64_t pc = 0x1000;
-        const uint32_t opcode = 0x80007057;
-        injectInstruction(pc, opcode);
+        const uint32_t rs1 = 1, rs2 = 2, rd = 3;
+        atlas::AtlasState* state = getAtlasState();
 
-        const atlas::AtlasState* state = getAtlasState();
+        WRITE_INT_REG<XLEN>(state, rs1, 8);            // avl = 8
+        XLEN vtype = setVtypeCSR<XLEN>(0, 0, 0, 0, 0); // vlmul = 1, sew = 8
+        WRITE_INT_REG<XLEN>(state, rs2, vtype);
+        instPtr_ = makeAtlasInst(std::string{"vsetvl"}, mavis::ExtractorIF::RegListType{rs1, rs2},
+                                 mavis::ExtractorIF::RegListType{rd});
+        executeInstruction(instPtr_);
+
+        EXPECT_EQUAL(READ_INT_REG<XLEN>(state, rd), 8);
         const atlas::AtlasState::SimState* sim_state = state->getSimState();
-
         std::cout << sim_state->current_inst << std::endl;
         EXPECT_EQUAL(sim_state->inst_count, 1);
     }
 
     void testVsetvli()
     {
-        const uint64_t pc = 0x1000;
-        const uint32_t opcode = 0x7057;
-        injectInstruction(pc, opcode);
+        const uint32_t rs1 = 1, rd = 3;
+        atlas::AtlasState* state = getAtlasState();
 
-        const atlas::AtlasState* state = getAtlasState();
+        WRITE_INT_REG<XLEN>(state, rs1, 8);            // avl = 8
+        XLEN vtype = setVtypeCSR<XLEN>(0, 0, 0, 0, 0); // vlmul = 1, sew = 8
+        instPtr_ = makeAtlasInst(std::string{"vsetvli"}, mavis::ExtractorIF::RegListType{rs1},
+                                 mavis::ExtractorIF::RegListType{rd}, vtype);
+        executeInstruction(instPtr_);
+
+        EXPECT_EQUAL(READ_INT_REG<XLEN>(state, rd), 8);
         const atlas::AtlasState::SimState* sim_state = state->getSimState();
-
         std::cout << sim_state->current_inst << std::endl;
         EXPECT_EQUAL(sim_state->inst_count, 2);
     }
 
     void testVsetivli()
     {
+        atlas::AtlasState* state = getAtlasState();
         const uint64_t pc = 0x1000;
-        const uint32_t opcode = 0xc0007057;
+        const uint32_t rd = 3;
+        uint32_t opcode;
+
+        opcode = vsetivliOp(rd, 8, 0, 0, 0, 0); // vl = 8, vlmul = 1, sew = 8
         injectInstruction(pc, opcode);
 
-        const atlas::AtlasState* state = getAtlasState();
-        const atlas::AtlasState::SimState* sim_state = state->getSimState();
+        EXPECT_EQUAL(READ_INT_REG<XLEN>(state, rd), 8);
 
+        opcode = vsetivliOp(rd, 16, 1, 0, 0, 0); // vl = 16, vlmul = 2, sew = 8
+        injectInstruction(pc, opcode);
+
+        EXPECT_EQUAL(READ_INT_REG<XLEN>(state, rd), 16);
+
+        const atlas::AtlasState::SimState* sim_state = state->getSimState();
         std::cout << sim_state->current_inst << std::endl;
-        EXPECT_EQUAL(sim_state->inst_count, 3);
+        EXPECT_EQUAL(sim_state->inst_count, 4);
     }
 
   private:
+    uint32_t vsetivliOp(uint8_t rd, uint8_t avl, uint8_t vlmul, uint8_t sew, uint8_t vta,
+                        uint8_t vma)
+    {
+        uint32_t opcode = 0;
+        uint8_t offset = 0;
+        opcode |= 0x57 << offset;
+        offset += 7;
+        opcode |= rd << offset;
+        offset += 5;
+        opcode |= 7 << offset;
+        offset += 3;
+        opcode |= avl << offset;
+        offset += 5;
+        opcode |= vlmul << offset;
+        offset += 3;
+        opcode |= sew << offset;
+        offset += 3;
+        opcode |= vta << offset;
+        offset += 1;
+        opcode |= vma << offset;
+        offset += 1;
+        offset += 2;
+        opcode |= 1 << offset;
+        offset += 1;
+        opcode |= 1 << offset;
+        offset += 1;
+        assert(offset == 32);
+        return opcode;
+    }
+
+    atlas::AtlasInst::PtrType instPtr_ = nullptr;
 };
 
 int main()
--- test/sim/via/Via_test.cpp
+++ test/sim/via/Via_test.cpp
@@ -1,35 +1,34 @@
 #include "test/sim/InstructionTester.hpp"
 #include "sparta/utils/SpartaTester.hpp"
 #include "core/VectorState.hpp"
+#include "mavis/Mavis.h"
 
 class ViaInstructionTester : public AtlasInstructionTester
 {
 
   public:
+    using VLEN = std::array<uint8_t, 8>;
+    using XLEN = uint64_t;
+
     ViaInstructionTester() = default;
 
     void testVaddvv1()
     {
-        using VLEN = std::array<uint8_t, 8>;
-        using XLEN = uint64_t;
-        uint64_t pc;
-        uint32_t opcode;
-        uint32_t rd = 1;
         atlas::AtlasState* state = getAtlasState();
-        state->getVectorState()->setVSTART(0);
 
-        pc = 0x1000;
-        opcode = vsetivliOp(rd, 8, 0, 0, 0, 0); // vl = 8, vlmul = 1, sew = 8
-        injectInstruction(pc, opcode);
-        EXPECT_EQUAL(READ_INT_REG<XLEN>(state, rd), 8);
+        state->getVectorState()->setVSTART(0);
+        state->getVectorState()->setVL(8);   // avl = 8
+        state->getVectorState()->setLMUL(8); // vlmul = 1
+        state->getVectorState()->setSEW(8);  // sew = 8
 
         VLEN vs1_val = {0, 1, 2, 3, 4, 5, 6, 7};
         VLEN vs2_val = {1, 2, 3, 4, 5, 6, 7, 8};
         VLEN vs3_val = {1, 3, 5, 7, 9, 11, 13, 15};
         WRITE_VEC_REG<VLEN>(state, 1, vs1_val);
         WRITE_VEC_REG<VLEN>(state, 2, vs2_val);
-        opcode = vaddvvOp(3, 2, 1); // vd = 3, vs1 = 2, vs2 = 1
-        injectInstruction(pc, opcode);
+        instPtr_ = makeAtlasInst(std::string{"vadd.vv"}, mavis::ExtractorIF::RegListType{1, 2},
+                                 mavis::ExtractorIF::RegListType{3}); // vd = 3, vs1 = 1, vs2 = 2
+        executeInstruction(instPtr_);
 
         auto vd_val = READ_VEC_REG<VLEN>(state, 3);
         for (size_t i = 0; i < vd_val.size(); ++i)
@@ -38,23 +37,17 @@ class ViaInstructionTester : public AtlasInstructionTester
         }
         const atlas::AtlasState::SimState* sim_state = state->getSimState();
         std::cout << sim_state->current_inst << std::endl;
-        EXPECT_EQUAL(sim_state->inst_count, 2);
+        EXPECT_EQUAL(sim_state->inst_count, 1);
     }
 
     void testVaddvv2()
     {
-        using VLEN = std::array<uint8_t, 8>;
-        using XLEN = uint64_t;
-        uint64_t pc;
-        uint32_t opcode;
-        uint32_t rd = 1;
         atlas::AtlasState* state = getAtlasState();
-        state->getVectorState()->setVSTART(0);
 
-        pc = 0x1000;
-        opcode = vsetivliOp(rd, 16, 1, 0, 0, 0); // vl = 16, vlmul = 2, sew = 8
-        injectInstruction(pc, opcode);
-        EXPECT_EQUAL(READ_INT_REG<XLEN>(state, rd), 16);
+        state->getVectorState()->setVSTART(0);
+        state->getVectorState()->setVL(16);   // avl = 16
+        state->getVectorState()->setLMUL(16); // vlmul = 2
+        state->getVectorState()->setSEW(8);   // sew = 8
 
         VLEN vs1_val = {0, 1, 2, 3, 4, 5, 6, 7};
         VLEN vs2_val = {1, 2, 3, 4, 5, 6, 7, 8};
@@ -66,8 +59,9 @@ class ViaInstructionTester : public AtlasInstructionTester
         WRITE_VEC_REG<VLEN>(state, 2, vs2_val);
         WRITE_VEC_REG<VLEN>(state, 3, vs3_val);
         WRITE_VEC_REG<VLEN>(state, 4, vs4_val);
-        opcode = vaddvvOp(5, 3, 1); // vd = 5, vs1 = 3, vs2 = 1
-        injectInstruction(pc, opcode);
+        instPtr_ = makeAtlasInst(std::string{"vadd.vv"}, mavis::ExtractorIF::RegListType{1, 3},
+                                 mavis::ExtractorIF::RegListType{5}); // vd = 5, vs1 = 1, vs2 = 3
+        executeInstruction(instPtr_);
 
         auto vd_val1 = READ_VEC_REG<VLEN>(state, 5);
         for (size_t i = 0; i < vd_val1.size(); ++i)
@@ -81,48 +75,11 @@ class ViaInstructionTester : public AtlasInstructionTester
         }
         const atlas::AtlasState::SimState* sim_state = state->getSimState();
         std::cout << sim_state->current_inst << std::endl;
-        EXPECT_EQUAL(sim_state->inst_count, 4);
+        EXPECT_EQUAL(sim_state->inst_count, 2);
     }
 
   private:
-    uint32_t vsetivliOp(uint8_t rd, uint8_t avl, uint8_t vlmul, uint8_t sew, uint8_t vta,
-                        uint8_t vma)
-    {
-        uint32_t opcode = 0;
-        uint8_t offset = 0;
-        opcode |= 0x57 << offset;
-        offset += 7;
-        opcode |= rd << offset;
-        offset += 5;
-        opcode |= 7 << offset;
-        offset += 3;
-        opcode |= avl << offset;
-        offset += 5;
-        opcode |= vlmul << offset;
-        offset += 3;
-        opcode |= sew << offset;
-        offset += 3;
-        opcode |= vta << offset;
-        offset += 1;
-        opcode |= vma << offset;
-        offset += 1;
-        offset += 2;
-        opcode |= 1 << offset;
-        offset += 1;
-        opcode |= 1 << offset;
-        offset += 1;
-        assert(offset == 32);
-        return opcode;
-    }
-
-    uint32_t vaddvvOp(uint8_t rd, uint8_t rs1, uint8_t rs2)
-    {
-        uint32_t opcode = 0x57;
-        opcode |= rd << 7;
-        opcode |= rs1 << 15;
-        opcode |= rs2 << 20;
-        return opcode;
-    }
+    atlas::AtlasInst::PtrType instPtr_ = nullptr;
 };
 
 int main()
