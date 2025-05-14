9b9a41b5e9e1a46b3743f4de4db73a5cf17222f6
colby-nyce <iheartridleyman@gmail.com>
2025-05-14 15:49:02 -0500

    Remove outdated cosim comparison code (#140)
    All of this code should be removed:
    - SimDB has been rewritten and these header files no longer exist.
    - This cosim comparison only supported the prototype Atlas IDE. No
    remaining code reads these tables.
    - The original cosim IDE was for Atlas/Imperas. We now have better
    support in the new IDE for Atlas/Spike.
    
    Rewriting this code just so that it compiles with the new SimDB doesn't
    seem like a good use of time since nothing depends on it except a
    deleted prototype UI.
    
    This also unblocks Ayesha so she can use MAP latest instead of the ~5
    week old MAP that still had the old SimDB.

--- core/Exception.cpp
+++ core/Exception.cpp
@@ -135,7 +135,6 @@ namespace atlas
         state->setPrivMode(priv_mode, prev_virt_mode);
         state->changeMMUMode<XLEN>();
 
-        state->snapshotAndSyncWithCoSim();
         fault_cause_.clearValid();
         interrupt_cause_.clearValid();
         return nullptr;
