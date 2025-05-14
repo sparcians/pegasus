578bfa21ad74f6f754e8f67902ac82c25bc4dd3f
Colby Nyce <iheartridleyman@gmail.com>
2025-05-14 12:11:52 -0500

    Remove outdated cosim comparison code

--- sim/AtlasSim.cpp
+++ sim/AtlasSim.cpp
@@ -3,20 +3,6 @@
 #include "include/CSRFieldIdxs64.hpp"
 #include <filesystem>
 
-// Core database headers
-#include "simdb/ObjectManager.hpp"
-#include "simdb/ObjectRef.hpp"
-#include "simdb/TableRef.hpp"
-#include "simdb/utils/ObjectQuery.hpp"
-#include "simdb/utils/BlobHelpers.hpp"
-#include "simdb/async/AsyncTaskEval.hpp"
-
-// SQLite-specific headers
-#include "simdb/impl/sqlite/SQLiteConnProxy.hpp"
-#include "simdb/impl/sqlite/TransactionUtils.hpp"
-#include "simdb/impl/sqlite/Errors.hpp"
-#include <sqlite3.h>
-
 namespace atlas
 {
     AtlasSim::AtlasSim(sparta::Scheduler* scheduler, const std::string & workload,
@@ -35,17 +21,6 @@ namespace atlas
         }
 
         getRoot()->enterTeardown();
-
-        if (cosim_db_)
-        {
-            cosim_db_->getTaskQueue()->flushQueue();
-            cosim_db_->getTaskQueue()->stopThread();
-
-            const std::string cmd = "mv " + cosim_db_->getDatabaseFile() + " "
-                                    + std::filesystem::path(workload_).filename().string() + ".wdb";
-            cosim_db_.reset();
-            std::ignore = system(cmd.c_str());
-        }
     }
 
     void AtlasSim::run(uint64_t run_time)
@@ -70,116 +45,6 @@ namespace atlas
         std::cout << "MIPS: " << std::dec << (inst_count / (sim_time / 1000000.0)) << std::endl;
 
         // TODO: mem usage, workload exit code
-    }
-
-    void AtlasSim::enableCoSimDebugger(std::unique_ptr<CoSimQuery> query)
-    {
-        using RegisterInfo = AtlasState::RegisterInfo;
-        using RegisterInfoByHart = std::unordered_map<uint32_t, std::vector<RegisterInfo>>;
-        RegisterInfoByHart reg_info_by_hart;
-
-        for (uint32_t hart = 0; hart < state_.size(); ++hart)
-        {
-            auto state = state_.at(hart);
-
-            auto int_rset = state->getIntRegisterSet();
-            for (uint32_t reg_idx = 0; reg_idx < query->getNumIntRegisters(); ++reg_idx)
-            {
-                auto reg = int_rset->getRegister(reg_idx);
-                const uint64_t expected = query->getIntRegValue(hart, reg->getID());
-                const uint64_t actual = reg->dmiRead<uint64_t>();
-                reg_info_by_hart[hart].push_back(std::make_tuple(reg->getName(), reg->getGroupNum(),
-                                                                 reg->getID(), expected, actual));
-            }
-
-            auto fp_rset = state->getFpRegisterSet();
-            for (uint32_t reg_idx = 0; reg_idx < query->getNumFpRegisters(); ++reg_idx)
-            {
-                auto reg = fp_rset->getRegister(reg_idx);
-                const uint64_t expected = query->getFpRegValue(hart, reg->getID());
-                const uint64_t actual = reg->dmiRead<uint64_t>();
-                reg_info_by_hart[hart].push_back(std::make_tuple(reg->getName(), reg->getGroupNum(),
-                                                                 reg->getID(), expected, actual));
-            }
-
-            auto vec_rset = state->getVecRegisterSet();
-            for (uint32_t reg_idx = 0; reg_idx < query->getNumVecRegisters(); ++reg_idx)
-            {
-                auto reg = vec_rset->getRegister(reg_idx);
-                const uint64_t expected = query->getVecRegValue(hart, reg->getID());
-                const uint64_t actual = reg->dmiRead<uint64_t>();
-                reg_info_by_hart[hart].push_back(std::make_tuple(reg->getName(), reg->getGroupNum(),
-                                                                 reg->getID(), expected, actual));
-            }
-
-            auto csr_rset = state->getCsrRegisterSet();
-            for (uint32_t reg_idx = 0; reg_idx < csr_rset->getNumRegisters(); ++reg_idx)
-            {
-                if (auto reg = csr_rset->getRegister(reg_idx))
-                {
-                    if (query->isCsrImplemented(reg->getName()))
-                    {
-                        const uint64_t expected = query->getCsrRegValue(hart, reg->getName());
-                        const uint64_t actual = reg->dmiRead<uint64_t>();
-                        reg_info_by_hart[hart].push_back(std::make_tuple(
-                            reg->getName(), reg->getGroupNum(), reg->getID(), expected, actual));
-                    }
-                }
-            }
-        }
-
-        cosim_query_.reset(query.release());
-
-        using dt = simdb::ColumnDataType;
-        simdb::Schema schema;
-
-        schema.addTable("Registers")
-            .addColumn("HartId", dt::int32_t)
-            .addColumn("RegName", dt::string_t)
-            .addColumn("RegType", dt::int32_t)
-            .addColumn("RegIdx", dt::int32_t)
-            .addColumn("ExpectedInitVal", dt::uint64_t)
-            .addColumn("ActualInitVal", dt::uint64_t);
-
-        schema.addTable("Instructions")
-            .addColumn("HartId", dt::int32_t)
-            .addColumn("Rs1", dt::string_t)
-            .addColumn("Rs1Val", dt::uint64_t)
-            .addColumn("Rs2", dt::string_t)
-            .addColumn("Rs2Val", dt::uint64_t)
-            .addColumn("Rd", dt::string_t)
-            .addColumn("RdValBefore", dt::uint64_t)
-            .addColumn("RdValAfter", dt::uint64_t)
-            .addColumn("TruthRdValAfter", dt::uint64_t)
-            .addColumn("HasImm", dt::int32_t)
-            .addColumn("Imm", dt::uint64_t)
-            .addColumn("Disasm", dt::string_t)
-            .addColumn("Mnemonic", dt::string_t)
-            .addColumn("Opcode", dt::uint64_t)
-            .addColumn("PC", dt::uint64_t)
-            .addColumn("Priv", dt::int32_t)
-            .addColumn("ResultCode", dt::int32_t);
-
-        schema.addTable("PostInstCsrVals")
-            .addColumn("HartId", dt::int32_t)
-            .addColumn("PC", dt::uint64_t)
-            .addColumn("CsrName", dt::string_t)
-            .addColumn("AtlasCsrVal", dt::uint64_t)
-            .addColumn("CosimCsrVal", dt::uint64_t);
-
-        cosim_db_.reset(new simdb::ObjectManager);
-        cosim_db_->disableWarningMessages();
-        cosim_db_->createDatabaseFromSchema(schema, std::make_unique<simdb::SQLiteConnProxy>());
-
-        cosim_db_->safeTransaction(
-            [&]()
-            {
-                for (uint32_t hart = 0; hart < state_.size(); ++hart)
-                {
-                    auto state = state_.at(hart);
-                    state->enableCoSimDebugger(cosim_db_, cosim_query_, reg_info_by_hart[hart]);
-                }
-            });
     }
 
     void AtlasSim::enableInteractiveMode()
