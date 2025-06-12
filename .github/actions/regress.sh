7d65b528648a9c552b92b3a22b266b13a1b09b12
Kathlene Magnus <kmagnus@mips.com>
2025-06-12 15:04:06 -0500

    Refactored Actions and Made Progress on Post-Merge Regression (#172)
    Co-authored-by: Kathlene Magnus <kathlene.hurt@gmail.com>

---
+++ .github/actions/regress.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+
+echo "Starting Build Entry"
+echo "HOME:" $HOME
+echo "GITHUB_WORKSPACE:" $GITHUB_WORKSPACE
+echo "GITHUB_EVENT_PATH:" $GITHUB_EVENT_PATH
+echo "PWD:" `pwd`
+
+cd ${GITHUB_WORKSPACE}
+cd $ATLAS_BUILD_TYPE
+make -j$(nproc --all) atlas_regress
+REGRESS_ATLAS=$?
+if [ ${REGRESS_ATLAS} -ne 0 ]; then
+    echo "ERROR: regress of Atlas FAILED!!!"
+    exit 1
+fi
