e1cf57aa4fbce92af80796f5903f9cbcfa7c84e7
Kathlene Magnus <kathlene.hurt@gmail.com>
2025-07-30 13:27:50 -0500

    Added comments

--- core/inst_handlers/m/RvmInsts.hpp
+++ core/inst_handlers/m/RvmInsts.hpp
@@ -14,26 +14,39 @@ namespace pegasus
         template <typename XLEN> static void getInstHandlers(Execute::InstHandlersMap &);
 
       private:
+        // div
         template <typename XLEN>
-        Action::ItrType div_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
+        Action::ItrType divHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
+
+        // divu
         template <typename XLEN>
-        Action::ItrType divu_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
-        Action::ItrType divuw_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
-        Action::ItrType divw_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
+        Action::ItrType divuHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
+
+        // divw (rv64 only)
+        Action::ItrType divuwHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
+
+        // divw (rv64 only)
+        Action::ItrType divwHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
+
+        // mul, mulh, mulhsu, mulhu
+        template <typename XLEN, typename OPERATOR>
+        Action::ItrType mulHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
+
+        // mulw (rv64 only)
+        Action::ItrType mulwHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
+
+        // rem
         template <typename XLEN>
-        Action::ItrType mul_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
+        Action::ItrType remHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
+
+        // remu
         template <typename XLEN>
-        Action::ItrType mulh_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
-        template <typename XLEN>
-        Action::ItrType mulhsu_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
-        template <typename XLEN>
-        Action::ItrType mulhu_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
-        Action::ItrType mulw_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
-        template <typename XLEN>
-        Action::ItrType rem_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
-        template <typename XLEN>
-        Action::ItrType remu_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
-        Action::ItrType remuw_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
-        Action::ItrType remw_64Handler_(pegasus::PegasusState* state, Action::ItrType action_it);
+        Action::ItrType remuHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
+
+        // remuw (rv64 only)
+        Action::ItrType remuwHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
+
+        // remw (rv64 only)
+        Action::ItrType remwHandler_(pegasus::PegasusState* state, Action::ItrType action_it);
     };
 } // namespace pegasus
