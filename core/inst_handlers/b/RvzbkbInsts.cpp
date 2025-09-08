3303d42b5e7cf1e49d41e735def16dc966c5246b
Elliott I. Gurrola <egurrola@mips.com>
2025-09-08 08:38:37 -0700

    Add ZBKB extension, also removed some duplicated code between ZBB, ZBS, ZBKB extensions. Cleanup ZBB duplicated handler registration

---
+++ core/inst_handlers/b/RvzbkbInsts.cpp
@@ -0,0 +1,60 @@
+#include "core/inst_handlers/b/RvzbkbInsts.hpp"
+#include "core/inst_handlers/b/RvbFunctors.hpp"
+#include "core/inst_handlers/i/RviFunctors.hpp"
+#include "core/inst_handlers/inst_helpers.hpp"
+#include "include/PegasusUtils.hpp"
+#include "include/ActionTags.hpp"
+#include "core/ActionGroup.hpp"
+#include "core/PegasusState.hpp"
+#include "core/PegasusInst.hpp"
+#include "system/PegasusSystem.hpp"
+
+namespace pegasus
+{
+    template <typename XLEN>
+    void RvzbkbInsts::getInstHandlers(std::map<std::string, Action> & inst_handlers)
+    {
+        static_assert(std::is_same_v<XLEN, RV64> || std::is_same_v<XLEN, RV32>);
+
+        inst_handlers.emplace(
+            "pack", pegasus::Action::createAction<
+                       &RvzbkbInsts::binaryOpHandler_<RV64, Pack<XLEN>>, RvzbInstsBase>(
+                       nullptr, "pack", ActionTags::EXECUTE_TAG));
+
+        inst_handlers.emplace(
+            "packh", pegasus::Action::createAction<
+                       &RvzbkbInsts::binaryOpHandler_<RV64, Packh<XLEN>>, RvzbInstsBase>(
+                       nullptr, "packh", ActionTags::EXECUTE_TAG));
+
+        if constexpr (std::is_same_v<XLEN, RV64>)
+        {
+            inst_handlers.emplace(
+                "packw", pegasus::Action::createAction<
+                           &RvzbkbInsts::binaryOpHandler_<RV64, Packw<XLEN>>, RvzbInstsBase>(
+                           nullptr, "packw", ActionTags::EXECUTE_TAG));
+        }
+
+        inst_handlers.emplace(
+            "brev8", pegasus::Action::createAction<
+                       &RvzbkbInsts::unaryOpHandler_<RV64, Brev8<XLEN>>, RvzbInstsBase>(
+                       nullptr, "brev8", ActionTags::EXECUTE_TAG));
+
+        if constexpr (std::is_same_v<XLEN, RV32>)
+        {
+            inst_handlers.emplace(
+                "unzip", pegasus::Action::createAction<
+                           &RvzbkbInsts::unaryOpHandler_<RV64, Unzip<XLEN>>, RvzbInstsBase>(
+                           nullptr, "unzip", ActionTags::EXECUTE_TAG));
+
+            inst_handlers.emplace(
+                "zip", pegasus::Action::createAction<
+                           &RvzbkbInsts::unaryOpHandler_<RV64, Zip<XLEN>>, RvzbInstsBase>(
+                           nullptr, "zip", ActionTags::EXECUTE_TAG));
+        }
+
+    }
+
+    template void RvzbkbInsts::getInstHandlers<RV32>(std::map<std::string, Action> &);
+    template void RvzbkbInsts::getInstHandlers<RV64>(std::map<std::string, Action> &);
+
+} // namespace pegasus
--- scripts/insts/RVZBKB_INST.py
+++ scripts/insts/RVZBKB_INST.py
@@ -1,20 +1,20 @@
 #   RV32 RV64 OTHER     Mnemonic    Instruction
 #   x	 x    Zbb       ANDN 	    AND with inverted operand
-#   x	 x    -         BREV8 	    Reverse bits in bytes
 #   x	 x    Zbb       ORN 	    OR with inverted operand
 #   x	 x    -         PACK 	    Pack low halves of registers
 #   x	 x    -         PACKH 	    Pack low bytes of registers
-#   	 x    -         PACKW 	    Pack low 16-bits of registers (RV64)
-#   x	 x    -         REV8 	    Byte-reverse register
+#   -    x    -         PACKW 	    Pack low 16-bits of registers (RV64)
+#   x	 x    Zbb        REV8 	    Byte-reverse register
+#   x	 x    -         BREV8 	    Reverse bits in bytes
 #   x    x    Zbb       ROL 	    Rotate left (Register)
-#   	 x    Zbb       ROLW 	    Rotate Left Word (Register)
+#   -    x    Zbb       ROLW 	    Rotate Left Word (Register)
 #   x	 x    Zbb       ROR 	    Rotate right (Register)
 #   x	 x    Zbb       RORI 	    Rotate right (Immediate)
-#   	 x    Zbb       RORIW 	    Rotate right Word (Immediate)
-#   	 x    Zbb       RORW 	    Rotate right Word (Register)
-#   x	      -         UNZIP 	    Bit deinterleave
+#   -    x    Zbb       RORIW 	    Rotate right Word (Immediate)
+#   -    x    Zbb       RORW 	    Rotate right Word (Register)
+#   x	 -    -         UNZIP 	    Bit deinterleave
 #   x	 x    Zbb       XNOR 	    Exclusive NOR
-#   x	      -         ZIP 	    Bit interleave
+#   x	 -    -         ZIP 	    Bit interleave
 
 
 RV32ZBKB_INST = [
