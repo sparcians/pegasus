83932fd6ff5c96da90f4229192668a31a8ebd56c
Colby Nyce <iheartridleyman@gmail.com>
2025-11-06 10:04:58 -0600

    Parameterize the checkpointer

--- cosim/PegasusCoSim.hpp
+++ cosim/PegasusCoSim.hpp
@@ -47,7 +47,9 @@ namespace pegasus::cosim
       public:
         PegasusCoSim(sparta::Scheduler* scheduler, uint64_t ilimit = 0,
                      const std::string & workload = "",
-                     const std::string & db_file = "pegasus-cosim.db");
+                     const std::string & db_file = "pegasus-cosim.db",
+                     const size_t snapshot_threshold = 100,
+                     const size_t max_cached_windows = 10);
 
         ~PegasusCoSim() noexcept;
 
