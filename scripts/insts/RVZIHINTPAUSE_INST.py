a5f48e39ec435c7a23b1cc78c26800fe6ad54be7
Kathlene Magnus <kathlene.hurt@gmail.com>
2025-11-20 12:00:42 -0600

    Fixed pause inst

--- scripts/insts/RVZIHINTPAUSE_INST.py
+++ scripts/insts/RVZIHINTPAUSE_INST.py
@@ -1,9 +1,9 @@
 RVZIHINTPAUSE_MAVIS_EXTS = ["zihintpause"]
 
 RV32ZIHINTPAUSE_INST = [
-  {'mnemonic': 'PAUSE'  , 'handler': 'pause', 'cost': 1, 'tags': 'HINT_EXT_32,HINT_EXT_64', 'memory': False, 'cof': False},   # fence p=w,s=0,rd=x0,rs1=x0    #TODO nop handler should be changed to a pause once multithread is supported
+  {'mnemonic': 'pause'  , 'handler': 'pause', 'cost': 1, 'tags': 'HINT_EXT_32,HINT_EXT_64', 'memory': False, 'cof': False},   # fence p=w,s=0,rd=x0,rs1=x0    #TODO nop handler should be changed to a pause once multithread is supported
 ]
 
 RV64ZIHINTPAUSE_INST = [
-  {'mnemonic': 'PAUSE'  , 'handler': 'pause', 'cost': 1, 'tags': 'HINT_EXT_32,HINT_EXT_64', 'memory': False, 'cof': False},   # fence p=w,s=0,rd=x0,rs1=x0    #TODO nop handler should be changed to a pause once multithread is supported
+  {'mnemonic': 'pause'  , 'handler': 'pause', 'cost': 1, 'tags': 'HINT_EXT_32,HINT_EXT_64', 'memory': False, 'cof': False},   # fence p=w,s=0,rd=x0,rs1=x0    #TODO nop handler should be changed to a pause once multithread is supported
 ]
