72ca2d8769117d5a0c118e67b377311eda56d906
Liya Zhu <liya.zhu@globalfoundries.com>
2026-02-20 18:41:27 -0600

    codre review 3

--- cosim/PegasusCoSim.hpp
+++ cosim/PegasusCoSim.hpp
@@ -31,7 +31,7 @@ namespace sparta
 
     namespace memory
     {
-        class BlockingMemoryIFNode;
+        class BlockingMemoryIF;
     }
 
     class Register;
@@ -50,7 +50,7 @@ namespace pegasus::cosim
     class CoSimMemoryInterface : public cosim::MemoryInterface
     {
       public:
-        CoSimMemoryInterface(sparta::memory::BlockingMemoryIFNode* memory_map);
+        CoSimMemoryInterface(sparta::memory::BlockingMemoryIF* memory_map);
 
         ~CoSimMemoryInterface() {}
 
@@ -64,7 +64,7 @@ namespace pegasus::cosim
                    std::vector<uint8_t> & buffer) const override;
 
       private:
-        sparta::memory::BlockingMemoryIFNode* memory_ = nullptr;
+        sparta::memory::BlockingMemoryIF* memory_ = nullptr;
     };
 
     class CoSimObserver;
