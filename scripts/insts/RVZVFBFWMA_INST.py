eecb59b1f199c4d705bd0cd9dbdc898b5384d47b
Kathlene Magnus <kathlene.hurt@gmail.com>
2026-03-04 11:24:02 -0600

    Hooked up zvfbfwma

---
+++ scripts/insts/RVZVFBFWMA_INST.py
@@ -0,0 +1,11 @@
+RVZVFBFWMA_MAVIS_EXTS = []
+
+RV32ZVFBFWMA_INST = [
+    {'mnemonic': 'vfwmaccbf16.vv', 'handler': 'unsupported', 'cost': 1, 'tags': 'ZVFBFWMA_EXT_32', 'memory': False, 'cof': False},
+    {'mnemonic': 'vfwmaccbf16.vf', 'handler': 'unsupported', 'cost': 1, 'tags': 'ZVFBFWMA_EXT_32', 'memory': False, 'cof': False},
+]
+
+RV64ZVFBFWMA_INST = [
+    {'mnemonic': 'vfwmaccbf16.vv', 'handler': 'unsupported', 'cost': 1, 'tags': 'ZVFBFWMA_EXT_64', 'memory': False, 'cof': False},
+    {'mnemonic': 'vfwmaccbf16.vf', 'handler': 'unsupported', 'cost': 1, 'tags': 'ZVFBFWMA_EXT_64', 'memory': False, 'cof': False},
+]
