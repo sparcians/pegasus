fc616c90eb46e870fa0b2e196eff980d1f196d0f
Kathlene Magnus <kathlene.hurt@gmail.com>
2026-03-04 12:59:39 -0600

    Hooked up zicbom, zicbop and ziboz

---
+++ core/inst_handlers/zicbom/RvzicbomInsts.cpp
@@ -0,0 +1,20 @@
+#include "core/inst_handlers/zicbom/RvzicbomInsts.hpp"
+#include "core/inst_handlers/i/RviInsts.hpp"
+#include "include/ActionTags.hpp"
+
+namespace pegasus
+{
+    template <typename XLEN>
+    void RvzicbomInsts::getInstHandlers(InstHandlers::InstHandlersMap & inst_handlers)
+    {
+        static_assert(std::is_same_v<XLEN, RV64> || std::is_same_v<XLEN, RV32>);
+
+        (void)inst_handlers;
+        //inst_handlers.emplace("cbo_clean", pegasus::Action::createAction<&RvzicbomInsts::, RvzicbomInsts>("cbo_clean", ActionTags::EXECUTE_TAG));
+        //inst_handlers.emplace("cbo_flush", pegasus::Action::createAction<&RvzicbomInsts::, RvzicbomInsts>("cbo_flush", ActionTags::EXECUTE_TAG));
+        //inst_handlers.emplace("cbo_inval", pegasus::Action::createAction<&RvzicbomInsts::, RvzicbomInsts>("cbo_inval", ActionTags::EXECUTE_TAG));
+    }
+
+    template void RvzicbomInsts::getInstHandlers<RV32>(InstHandlers::InstHandlersMap &);
+    template void RvzicbomInsts::getInstHandlers<RV64>(InstHandlers::InstHandlersMap &);
+} // namespace pegasus
---
+++ core/inst_handlers/zicboz/RvzicbozInsts.cpp
@@ -0,0 +1,18 @@
+#include "core/inst_handlers/zicboz/RvzicbozInsts.hpp"
+#include "core/inst_handlers/i/RviInsts.hpp"
+#include "include/ActionTags.hpp"
+
+namespace pegasus
+{
+    template <typename XLEN>
+    void RvzicbozInsts::getInstHandlers(InstHandlers::InstHandlersMap & inst_handlers)
+    {
+        static_assert(std::is_same_v<XLEN, RV64> || std::is_same_v<XLEN, RV32>);
+
+        (void)inst_handlers;
+        //inst_handlers.emplace("cbo_zero", pegasus::Action::createAction<&RvzicbozInsts::, RvzicbozInsts>("cbo_zero", ActionTags::EXECUTE_TAG));
+    }
+
+    template void RvzicbozInsts::getInstHandlers<RV32>(InstHandlers::InstHandlersMap &);
+    template void RvzicbozInsts::getInstHandlers<RV64>(InstHandlers::InstHandlersMap &);
+} // namespace pegasus
