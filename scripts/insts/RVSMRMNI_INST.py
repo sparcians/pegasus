86a68f86a88a97289724e6d33b9877d6fb31c3ae
Knute Lingaard <klingaard@mips.com>
2026-03-25 16:41:35 -0500

    Corrected spelling

--- scripts/insts/RVSMRMNI_INST.py
+++ scripts/insts/RVSMRMNI_INST.py
@@ -1,9 +1,9 @@
-RVSMRMNI_MAVIS_EXTS = ["smrmni"]
+RVSMRNMI_MAVIS_EXTS = ["smrnmi"]
 
-RV32SMRMNI_INST = [
-    {'mnemonic': 'mnret', 'handler': 'unsupported', 'cost': 1, 'tags': 'SMRMNI_EXT_32', 'memory': False, 'cof': True},
+RV32SMRNMI_INST = [
+    {'mnemonic': 'mnret', 'handler': 'unsupported', 'cost': 1, 'tags': 'SMRNMI_EXT_32', 'memory': False, 'cof': True},
 ]
 
-RV64SMRMNI_INST = [
-    {'mnemonic': 'mnret', 'handler': 'unsupported', 'cost': 1, 'tags': 'SMRMNI_EXT_64', 'memory': False, 'cof': True},
+RV64SMRNMI_INST = [
+    {'mnemonic': 'mnret', 'handler': 'unsupported', 'cost': 1, 'tags': 'SMRNMI_EXT_64', 'memory': False, 'cof': True},
 ]
