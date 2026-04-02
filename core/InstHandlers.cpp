f71f8fefaa00a86c46349db3d11798821ae4bb3b
Liya Zhu <liya.zhu@globalfoundries.com>
2026-04-02 17:48:44 -0500

    implement zvbc extension

--- core/InstHandlers.cpp
+++ core/InstHandlers.cpp
@@ -22,6 +22,7 @@
 #include "core/inst_handlers/v/RvvPermuteInsts.hpp"
 #include "core/inst_handlers/v/RvvReductionInsts.hpp"
 #include "core/inst_handlers/v/RvvZvbbInsts.hpp"
+#include "core/inst_handlers/v/RvvZvbcInsts.hpp"
 #include "core/inst_handlers/zabha/RvzabhaInsts.hpp"
 #include "core/inst_handlers/zacas/RvzacasInsts.hpp"
 #include "core/inst_handlers/zawrs/RvzawrsInsts.hpp"
@@ -41,7 +42,6 @@
 #include "core/inst_handlers/zihintpause/RvzihintpauseInsts.hpp"
 #include "core/inst_handlers/zilsd/RvzilsdInsts.hpp"
 #include "core/inst_handlers/zknd/RvzkndInsts.hpp"
-#include "core/inst_handlers/zvbc/RvzvbcInsts.hpp"
 #include "core/inst_handlers/zvfbfwma/RvzvfbfwmaInsts.hpp"
 #include "core/inst_handlers/zvkg/RvzvkgInsts.hpp"
 #include "core/inst_handlers/zvkned/RvzvknedInsts.hpp"
@@ -89,6 +89,7 @@ namespace pegasus
         RvvPermuteInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvvReductionInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvvZvbbInsts::getInstHandlers<RV64>(rv64_inst_actions_);
+        RvvZvbcInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvzabhaInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvzacasInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvzawrsInsts::getInstHandlers<RV64>(rv64_inst_actions_);
@@ -112,7 +113,7 @@ namespace pegasus
         RvzifenceiInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvzihintpauseInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvzkndInsts::getInstHandlers<RV64>(rv64_inst_actions_);
-        RvzvbcInsts::getInstHandlers<RV64>(rv64_inst_actions_);
+        RvvZvbcInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvzvfbfwmaInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvzvkgInsts::getInstHandlers<RV64>(rv64_inst_actions_);
         RvzvknedInsts::getInstHandlers<RV64>(rv64_inst_actions_);
@@ -138,6 +139,7 @@ namespace pegasus
         RvvPermuteInsts::getInstHandlers<RV32>(rv32_inst_actions_);
         RvvReductionInsts::getInstHandlers<RV32>(rv32_inst_actions_);
         RvvZvbbInsts::getInstHandlers<RV32>(rv32_inst_actions_);
+        RvvZvbcInsts::getInstHandlers<RV32>(rv32_inst_actions_);
         RvzabhaInsts::getInstHandlers<RV32>(rv32_inst_actions_);
         RvzacasInsts::getInstHandlers<RV32>(rv32_inst_actions_);
         RvzawrsInsts::getInstHandlers<RV32>(rv32_inst_actions_);
@@ -161,7 +163,7 @@ namespace pegasus
         RvzihintpauseInsts::getInstHandlers<RV32>(rv32_inst_actions_);
         RvzilsdInsts::getInstHandlers<RV32>(rv32_inst_actions_); // RV32 only
         RvzkndInsts::getInstHandlers<RV32>(rv32_inst_actions_);
-        RvzvbcInsts::getInstHandlers<RV32>(rv32_inst_actions_);
+        RvvZvbcInsts::getInstHandlers<RV32>(rv32_inst_actions_);
         RvzvfbfwmaInsts::getInstHandlers<RV32>(rv32_inst_actions_);
         RvzvkgInsts::getInstHandlers<RV32>(rv32_inst_actions_);
         RvzvknedInsts::getInstHandlers<RV32>(rv32_inst_actions_);
---
+++ core/inst_handlers/v/RvvZvbcInsts.cpp
@@ -0,0 +1,52 @@
+#include "core/inst_handlers/v/RvvZvbcInsts.hpp"
+#include "core/inst_handlers/v/RvvZvbcFunctors.hpp"
+#include "include/ActionTags.hpp"
+
+namespace pegasus
+{
+    template <typename XLEN>
+    void RvvZvbcInsts::getInstHandlers(InstHandlers::InstHandlersMap & inst_handlers)
+    {
+        static_assert(std::is_same_v<XLEN, RV64> || std::is_same_v<XLEN, RV32>);
+
+        inst_handlers.emplace(
+            "vclmul.vv",
+            pegasus::Action::createAction<
+                &RvvZvbcInsts::viBinaryHandler_<XLEN,
+                                                OperandMode{.dst = OperandMode::Mode::V,
+                                                            .src2 = OperandMode::Mode::V,
+                                                            .src1 = OperandMode::Mode::V},
+                                                false, CarrylessMultiply>,
+                RvvIntegerInsts>(nullptr, "vclmul.vv", ActionTags::EXECUTE_TAG));
+        inst_handlers.emplace(
+            "vclmul.vx",
+            pegasus::Action::createAction<
+                &RvvZvbcInsts::viBinaryHandler_<XLEN,
+                                                OperandMode{.dst = OperandMode::Mode::V,
+                                                            .src2 = OperandMode::Mode::V,
+                                                            .src1 = OperandMode::Mode::X},
+                                                false, CarrylessMultiply>,
+                RvvIntegerInsts>(nullptr, "vclmul.vx", ActionTags::EXECUTE_TAG));
+        inst_handlers.emplace(
+            "vclmulh.vv",
+            pegasus::Action::createAction<
+                &RvvZvbcInsts::viBinaryHandler_<XLEN,
+                                                OperandMode{.dst = OperandMode::Mode::V,
+                                                            .src2 = OperandMode::Mode::V,
+                                                            .src1 = OperandMode::Mode::V},
+                                                false, CarrylessMultiplyHigh>,
+                RvvIntegerInsts>(nullptr, "vclmulh.vv", ActionTags::EXECUTE_TAG));
+        inst_handlers.emplace(
+            "vclmulh.vx",
+            pegasus::Action::createAction<
+                &RvvZvbcInsts::viBinaryHandler_<XLEN,
+                                                OperandMode{.dst = OperandMode::Mode::V,
+                                                            .src2 = OperandMode::Mode::V,
+                                                            .src1 = OperandMode::Mode::X},
+                                                false, CarrylessMultiplyHigh>,
+                RvvIntegerInsts>(nullptr, "vclmulh.vx", ActionTags::EXECUTE_TAG));
+    }
+
+    template void RvvZvbcInsts::getInstHandlers<RV32>(InstHandlers::InstHandlersMap &);
+    template void RvvZvbcInsts::getInstHandlers<RV64>(InstHandlers::InstHandlersMap &);
+} // namespace pegasus
