022f824c774a009cc053c512864097004436cea2
Knute Lingaard <klingaard@mips.com>
2026-07-01 12:53:29 -0500

    Updated tests moving sections around

--- test/elfs/asm/vmask/vmask.s
+++ test/elfs/asm/vmask/vmask.s
@@ -10,13 +10,13 @@
     .global main
 
 main:
-    vsetvli t0, a0, e8, m1, ta, ma
+    vsetvli t0, x0, e8, m1, ta, ma
     vmandn.mm	v14,v30,v22
-    vsetvli t0, a0, e8, m2, ta, ma
+    vsetvli t0, x0, e8, m2, ta, ma
     vmandn.mm	v14,v30,v22
-    vsetvli t0, a0, e8, m4, ta, ma
+    vsetvli t0, x0, e8, m4, ta, ma
     vmandn.mm	v14,v30,v22
-    vsetvli t0, a0, e8, m8, ta, ma
+    vsetvli t0, x0, e8, m8, ta, ma
     vmandn.mm	v14,v30,v22
 
 pass:
